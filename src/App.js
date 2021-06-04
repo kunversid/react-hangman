c327d4f337f30e223341a860caa62b2c85e93556
Kunver Siddharth <kunversiddharth@outlook.com>
2021-06-04 22:10:02 +0530

    Notifications

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
-import Figure from "./components/Figure";
 import Header from "./components/Header";
-import Word from "./components/Word";
+import Figure from "./components/Figure";
 import WrongLetters from "./components/WrongLetters";
+import Word from "./components/Word";
+import Notification from "./components/Notification";
+import { showNotification as show } from "./helpers/helpers";
+
+import "./App.css";
 
 const words = ["acyclovir", "azithromycin", "‎amoxicillin", "alemtuzumab"];
 let selectedWord = words[Math.floor(Math.random() * words.length)];
@@ -12,6 +15,7 @@ function App() {
   const [playable, setPlayable] = useState(true);
   const [correctLetters, setCorrectLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
 
   useEffect(() => {
     const handleKeydown = (event) => {
@@ -22,13 +26,13 @@ function App() {
           if (!correctLetters.includes(letter)) {
             setCorrectLetters((currentLetters) => [...currentLetters, letter]);
           } else {
-            // show(setShowNotification);
+            show(setShowNotification);
           }
         } else {
           if (!wrongLetters.includes(letter)) {
             setWrongLetters((currentLetters) => [...currentLetters, letter]);
           } else {
-            // show(setShowNotification);
+            show(setShowNotification);
           }
         }
       }
@@ -46,6 +50,7 @@ function App() {
         <WrongLetters wrongLetters={wrongLetters} />
         <Word selectedWord={selectedWord} correctLetters={correctLetters} />
       </div>
+      <Notification showNotification={showNotification} />
     </>
   );
 }
