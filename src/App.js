74cfc18750a90384b867f786f3b37ecd7662aec7
Kunver Siddharth <kunversiddharth@outlook.com>
2021-06-04 18:49:08 +0530

    Adding Word Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Figure from "./components/Figure";
 import Header from "./components/Header";
+import Word from "./components/Word";
 import WrongLetters from "./components/WrongLetters";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <div className="game-container">
         <Figure />
         <WrongLetters />
+        <Word />
       </div>
     </>
   );
